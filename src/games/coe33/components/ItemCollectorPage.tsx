342c5aa3cc251928a502fc1b629227a67cd1816f
Josh Payette <hello@joshpayette.dev>
2025-08-30 13:33:56 -0400

    integrate item search and filtering into ItemCollectorPage

--- src/games/coe33/components/ItemCollectorPage.tsx
+++ src/games/coe33/components/ItemCollectorPage.tsx
@@ -1,27 +1,76 @@
 'use client';
 
-import { Flex } from '@mantine/core';
+import { Box, Flex, Stack } from '@mantine/core';
 import { motion } from 'framer-motion';
+import { useQueryState } from 'nuqs';
+import { useMemo, useState } from 'react';
 import { allGameConfigs } from '@/features/game/constants';
 import type { GameConfig } from '@/features/game/types';
 import { CompactItemCard } from '@/features/item/components/CompactItemCard';
+import { SearchItemInput } from '@/features/item/components/SearchItemInput';
+import { searchParser } from '@/features/item/search-params';
 import type { COE33ItemType } from '@/games/coe33/items/types';
 import { getImageUrl } from '@/utils/url';
+
+const findNewItems = (
+  items: COE33ItemType[],
+  query: string,
+  defaultItems: COE33ItemType[]
+): COE33ItemType[] => {
+  if (!query) {
+    return defaultItems;
+  }
+
+  const filteredItems = items
+    ?.filter((item) =>
+      item.name.toLowerCase().includes(query.trim().toLowerCase())
+    )
+    .filter(
+      (item, index, self) =>
+        index === self.findIndex((t) => t.slug === item.slug)
+    );
+
+  return filteredItems;
+};
 
 const ItemCollectorPage = () => {
   const gameConfig = allGameConfigs.find(
     (config): config is GameConfig<COE33ItemType> => config.id === 'coe33'
   );
 
-  if (!gameConfig) {
+  if (!gameConfig || !gameConfig.items) {
     throw new Error('Game configuration not found for COE33!');
   }
 
+  const defaultItems = gameConfig.items;
+
+  const [search, setSearch] = useQueryState('search', searchParser);
+  const [loading, setLoading] = useState(false);
+
+  const items = useMemo(() => {
+    setLoading(true);
+    const newItems = findNewItems(
+      gameConfig?.items ?? [],
+      search,
+      defaultItems
+    );
+    setLoading(false);
+    return newItems;
+  }, [gameConfig?.items, search, defaultItems]);
+
   return (
-    <Flex wrap="wrap" align="center" justify="center" gap="sm">
-      {gameConfig.items
-        ?.filter((item) => item.category === 'WEAPON')
-        .map((item) => (
+    <Stack>
+      <Box maw={600}>
+        <SearchItemInput
+          key={search}
+          items={items}
+          searchValue={search}
+          onSearchChange={setSearch}
+          loading={loading}
+        />
+      </Box>
+      <Flex wrap="wrap" align="center" justify="center" gap="sm">
+        {items.map((item) => (
           <motion.div
             key={item.slug}
             initial={{ opacity: 0, y: 10 }}
@@ -33,7 +82,8 @@ const ItemCollectorPage = () => {
             />
           </motion.div>
         ))}
-    </Flex>
+      </Flex>
+    </Stack>
   );
 };
 
